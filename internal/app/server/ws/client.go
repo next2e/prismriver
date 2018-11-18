84f3f0237c0909372fc281cb9d1ffe4d7984640f
Michael William Le Nguyen <michael@mail.ttp.codes>
2018-11-17 22:40:20 -0500

    Add base WebSocket structures

---
+++ internal/app/server/ws/client.go
@@ -0,0 +1,72 @@
+package ws
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+const (
+	pingInterval = pongWait * 9 / 10
+
+	pongWait = 60 * time.Second
+
+	writeWait = 10 * time.Second
+)
+
+type Client struct {
+	Conn *websocket.Conn
+
+	Hub *Hub
+
+	Send chan []byte
+}
+
+func (c *Client) RunRead() {
+	defer func() {
+		c.Hub.Unregister <- c
+		c.Conn.Close()
+	}()
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error { c.Conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		if _, _, err := c.Conn.NextReader(); err != nil {
+			c.Conn.Close()
+			break
+		}
+	}
+}
+
+func (c *Client) RunWrite() {
+	ticker := time.NewTicker(pingInterval)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+	for {
+		select{
+		case message, ok := <-c.Send:
+			logrus.Debug("Received Send message on WS Client.")
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
