a30ad41cd7815c9af0111c8a4f240a63ef91cec4
Michael William Le Nguyen <michael@mail.ttp.codes>
2018-11-19 03:09:57 -0500

    Add DB saving and reading functionality
    - The reading functionality should've been in a different commit, but
    guess what?
    - Git is being STUPID AS HELL at 3AM and not returning the proper
    changes.
    - FOR GOD'S SAKE, JETBRAINS WAS ABLE TO CORRECTLY IDENTIFY THE DAMN
    DIFF.
    - Anyways, yeah, messy commit. Ugh.

---
+++ internal/app/db/db.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"sync"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var db *gorm.DB
+var err error
+var once sync.Once
+
+func GetDatabase() (*gorm.DB, error) {
+	once.Do(func () {
+		newDb, openErr := gorm.Open("postgres", "host=localhost port=5432 user=postgres " +
+			"password=postgres dbname=postgres sslmode=disable")
+		err = openErr
+		db = newDb
+		if err != nil {
+			return
+		}
+		db.AutoMigrate(&Media{})
+	})
+	return db, err
+}
+
+func AddMedia(media Media) error {
+	db, err := GetDatabase()
+	if err != nil {
+		return err
+	}
+	db.Create(&media)
+	return nil
+}
+
+func GetMedia(id string, kind string) (Media, error) {
+	db, err := GetDatabase()
+	if err != nil {
+		logrus.Fatal("Error loading database:", err)
+	}
+	var media []Media
+	db.Where(Media{ID: id, Type: kind}).First(&media)
+	if len(media) > 0 {
+		return media[0], nil
+	}
+	return Media{}, errors.New("Media not found in DB.")
+}
--- internal/app/server/routes/queue/store.go
+++ internal/app/server/routes/queue/store.go
@@ -2,26 +2,45 @@ package queue
 
 import (
 	"github.com/sirupsen/logrus"
+	"gitlab.com/ttpcodes/prismriver/internal/app/db"
 	"gitlab.com/ttpcodes/prismriver/internal/app/player"
 	"gitlab.com/ttpcodes/prismriver/internal/app/sources"
 	"net/http"
+	"regexp"
 )
 
 func StoreHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-
+	id := r.Form.Get("id")
+	kind := r.Form.Get("type")
 	url := r.Form.Get("url")
-	if len(url) == 0 {
-		logrus.Warn("User sent an empty POST request, ignoring.")
-	} else {
-		queue := player.GetQueue()
-		media, err := sources.GetInfo(url)
-		if err != nil {
-			logrus.Error("Could not get video info")
+	if len(id) > 0 && len(kind) > 0 {
+		media, err := db.GetMedia(id, kind)
+		if err == nil {
+			queue := player.GetQueue()
+			queue.Add(media)
 			return
 		}
-		queue.Add(media)
-		logrus.Info("Added " + media.Title + " to queue.")
 	}
-
+	if len(url) != 0 {
+		regex := regexp.MustCompile(`(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)`)
+		res := regex.FindAllStringSubmatch(url, -1)
+		if len(res[0]) > 0 {
+			queue := player.GetQueue()
+			media, err := db.GetMedia(res[0][5], "youtube")
+			if err == nil {
+				queue.Add(media)
+				return
+			}
+			media, err = sources.GetInfo(url)
+			if err != nil {
+				logrus.Error("Could not get video info")
+				return
+			}
+			db.AddMedia(media)
+			queue.Add(media)
+			return
+		}
+	}
+	logrus.Warn("User sent an empty POST request, ignoring.")
 }
