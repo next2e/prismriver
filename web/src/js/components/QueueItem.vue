19924127b379c976ec200176948bd13a9999dd68
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-01-05 19:01:59 -0500

    Clean up QueueItem CSS
    - Replace text with glyphicons
    - Use disabled attribute instead of v-if on unusable controls
    - Clean up Vue styling syntax

--- web/src/js/components/QueueItem.vue
+++ web/src/js/components/QueueItem.vue
@@ -1,13 +1,13 @@
 <template>
   <li>
     <span>
-      <button @click="deleteSong" class="delete"> Delete </button>
-      <button v-if="!disableup" @click="up" class="select-song">Up</button>
-      <button v-if="!disabledown" @click="down" class="select-song">Down</button>
+      <button @click="deleteSong" class="delete"><span class="glyphicon glyphicon-trash"></span></button>
+      <button :disabled="disableup" @click="up" class="select-song pull-right"><span class="glyphicon glyphicon-arrow-up"></span></button>
+      <button :disabled="disabledown" @click="down" class="select-song pull-right" style="margin-left: 15px;"><span class="glyphicon glyphicon-arrow-down"></span></button>
       {{ item.Media.Title }}
     </span>
     <div v-if="item.Downloading" class="progress" style="margin-top: 8px; height: 2vh; font-size: 12px; text-align: center; line-height: 100%;">
-      <div class="progress-bar" :class="item.DownloadProgress < 50 ? 'progress-bar-info' : 'progress-bar-success'" style="min-width: 2vw;" :style="'width: ' + item.DownloadProgress + '%'"></div>
+      <div class="progress-bar" :class="item.DownloadProgress < 50 ? 'progress-bar-info' : 'progress-bar-success'" style="min-width: 2vw;" :style="{width: item.DownloadProgress + '%'}"></div>
     </div>
   </li>
 </template>
